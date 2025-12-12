152798c14c229e33058cb9c8e71187c7c4a6468d
Oscar Mas <oscarmascuevas@gmail.com>
2025-12-12 12:55:59 +0100

    .

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,10 @@ export default defineConfig(({ mode }) => ({
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, "")
       }
-    }
+    },
+    allowedHosts: [
+      "*.netlify.app",
+      "localhost"
+    ]
   }
 }));
